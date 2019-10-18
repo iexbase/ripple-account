44eb727fe0e553c1c79e4bdef887846077235fd8
serderovsh <steein.shamsudin@gmail.com>
2019-10-18 11:36:05 +0300

    update

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
+const express = require('express');
+const path = require('path');
+
+let app = express();
+const PORT = process.env.PORT || 30001;
+
+var Twig = require('twig'),
+    twig = Twig.twig;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("twig options", {
+    allow_async: true,
+    strict_variables: false
+});
 
 
-const RippleAPI = require('ripple-lib').RippleAPI;
-const api = new RippleAPI();
+app.get('/', function(req, res)
+{
+    const RippleAPI = require('ripple-lib').RippleAPI;
+    const api = new RippleAPI();
 
-console.log('Create Ripple account...');
+    let account = api.generateAddress();
+    res.render('index.twig', {
+        address: account.address,
+        private_key: account.secret
+    });
+});
 
-let account = api.generateAddress();
 
-console.log('----------------------------------');
-console.log('Address: ' + account.address);
-console.log('Secret key: ' + account.secret);
-console.log('----------------------------------');
+app.get('*', (req, res) =>
+{
+    global['navigator'] = req['headers']['user-agent'];
+    const http = req.headers['x-forwarded-proto'] === undefined ? 'http' : req.headers['x-forwarded-proto'];
+    const url = req.originalUrl;
+    // tslint:disable-next-line:no-console
+    console.time(`GET: ${url}`);
+});
 
-console.log('Account created successfully');
-
+app.listen(PORT, () => {
+    console.log(`listening on http://localhost:${PORT}!`);
+});
